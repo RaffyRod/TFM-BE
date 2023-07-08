59b14e0e612057c003b41ca5ff4ab985c239b9ca
Michael Ventura <michaelrafaelventura01@gmail.com>
2023-07-07 21:40:36 -0400

    primer commit para la api

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3030
+const ingredientes = require("./routes/ingredientes")
+const precios = require("./routes/precios")
+const recetas = require("./routes/recetas")
+const valoresNutricionales = require("./routes/valoresNutricionales")
+
+
+
+app.use('/ingredientes',ingredientes)
+app.use('/precios',precios)
+app.use('/recetas',recetas)
+app.use('/valores',valoresNutricionales)
+
+app.listen(port, ()=>{
+    console.log('escuchando el puerto ', port)
+})
