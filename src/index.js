85df386abd8f990ce451e6b24c96fb70e9f614df
Michael Ventura <michaelrafaelventura01@gmail.com>
2023-07-07 22:00:07 -0400

    nuevos cambios

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const ingredientes = require("./routes/ingredientes")
 const precios = require("./routes/precios")
 const recetas = require("./routes/recetas")
 const valoresNutricionales = require("./routes/valoresNutricionales")
+const usuarios = require('./routes/usuarios')
 
 
 
@@ -12,6 +13,7 @@ app.use('/ingredientes',ingredientes)
 app.use('/precios',precios)
 app.use('/recetas',recetas)
 app.use('/valores',valoresNutricionales)
+app.use('/usuarios',usuarios)
 
 app.listen(port, ()=>{
     console.log('escuchando el puerto ', port)
--- src/routes/ingredientes.js
+++ src/routes/ingredientes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
+// obtener todos los precios
 router.get("/",(req, res)=>{
     res.json({
         mensaje: "ingredientes"
